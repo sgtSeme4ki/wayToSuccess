2cdcb482bde2b67f6b162ff7daf1e7866e615862
sgtSeme4ki <markdurst98@gmail.com>
2020-11-03 19:27:57 +0100

    couplingRods now redraw on resize, but only for 3 nodes

--- couplingRod.js
+++ couplingRod.js
@@ -55,18 +55,121 @@ var lines = document.getElementsByTagName("line");
 //necessary to get lines drawn
 svg.innerHTML += "";
 
+window.addEventListener("resize", redraw, false);
 
-//console.log(assigned);
+
+//div3 950/434
+
+function redraw(e) {
+
+    assigned = new Map();
+    lines = null;
+
+    while (svg.firstChild) {
+        svg.removeChild(svg.lastChild);
+    }
+
+    for (i = 0; i < items.length - 1; i++) {
+        if (i === 0) {
+            //append first with last
+            let line = document.createElement("line");
+            line.className = "couplingRod";
+
+            let x1 = items.item(0).offsetLeft + (items.item(0).clientHeight / 2);
+            let y1 = items.item(0).offsetTop + (items.item(0).clientWidth / 2);
+            let x2 = items.item(items.length - 1).offsetLeft + (items.item(items.length - 1).clientHeight / 2);
+            let y2 = items.item(items.length - 1).offsetTop + (items.item(items.length - 1).clientWidth / 2);
+
+            test = items.item(0).getAttribute("style");
+            match = test.match(/-?\d+/g).map(Number);
 
 
-/*container.addEventListener("dragStart");
-container.addEventListener("dragEnd", dragEnd, false);
-container.addEventListener("drag", drag, false);*/
-// with eventlistner? callbacks | if position is
+            if (match.length > 2) {
+                
+                x1offset = match[2];
+                y1offset = match[3];
 
-//TODO: Write Code for arbitrary amount of nodes
-//TODO: Update couplingRods with Position of item
+                x1 = x1 + x1offset;
+                y1 = y1 + y1offset;
+            }
 
-//1: listen for movement of item
-//2: while item is dragged: get all lines which have x1|y1 or x2|y2 as middle position of item
-//3: update position of line(translate? | setAttribute?)
+            test = items.item(items.length - 1).getAttribute("style");
+            match = test.match(/-?\d+/g).map(Number);
+
+
+            if (match.length > 2) {
+
+                x2offset = match[2];
+                y2offset = match[3];
+
+                x2 = (x2 + x2offset);
+                y2 = (y2 + y2offset);
+
+            }
+            
+
+            line.setAttribute("x1", x1);
+            line.setAttribute("y1", y1);
+            line.setAttribute("x2", x2);
+            line.setAttribute("y2", y2);
+
+            assigned.set(line, [items.item(0), items.item(items.length - 1)]);
+            //assigned.set(items.item(0), [line], items.item(items.length - 1), [line]);
+
+            repl = line.outerHTML.replace("></line>", "/>");
+            svg.appendChild(line);
+            //console.log(repl);
+            //svg.innerHTML += repl;
+        }
+        // append rest with rest
+        let line = document.createElement("line");
+        line.className = "couplingRod";
+
+        let x1 = items.item(i).offsetLeft + (items.item(i).clientHeight / 2);
+        let y1 = items.item(i).offsetTop + (items.item(i).clientWidth / 2);
+        let x2 = items.item(i + 1).offsetLeft + (items.item(i + 1).clientHeight / 2);
+        let y2 = items.item(i + 1).offsetTop + (items.item(i + 1).clientWidth / 2);
+        // console.log(i);
+
+        test = items.item(i).getAttribute("style");
+        match = test.match(/-?\d+/g).map(Number);
+
+
+        if (match.length > 2) {
+            x1offset = match[2];
+            y1offset = match[3];
+
+            x1 = x1 + x1offset;
+            y1 = y1 + y1offset;
+        }
+
+        test = items.item(i + 1).getAttribute("style");
+        match = test.match(/-?\d+/g).map(Number);
+
+
+        if (match.length > 2) {
+            console.log("2 = " + match);
+            x2offset = match[2];
+            y2offset = match[3];
+
+            x2 = x2 + x2offset;
+            y2 = y2 + y2offset;
+
+        }
+
+        line.setAttribute("x1", x1);
+        line.setAttribute("y1", y1);
+        line.setAttribute("x2", x2);
+        line.setAttribute("y2", y2);
+
+        assigned.set(line, [items.item(i), items.item(i + 1)]);
+        //assigned.get(line).push()
+        svg.appendChild(line);
+    }
+    lines = document.getElementsByTagName("line");
+    //necessary to get lines drawn
+    svg.innerHTML += "";
+
+
+
+}
